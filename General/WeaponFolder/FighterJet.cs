a6542f84bed5787952710a34edb76a8f8ddb2af7
nachum <nahum0583@gmail.com>
2025-05-22 12:55:54 +0300

    yarchy asked changes

---
+++ General/WeaponFolder/FighterJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class FighterJet : Weapon
+{
+
+    public override string Name { get; set; }
+    public override int AmmunitionCapacity { get; set; }
+    public override int Energy { get; set; }
+    public override string TypeOfBomb { get; set; }
+
+    public FighterJet(string name, int ammunitionCapacity, int energy, string typeOfBomb)
+    {
+        Name = name;
+        AmmunitionCapacity = ammunitionCapacity;
+        Energy = energy;
+        TypeOfBomb = typeOfBomb;
+       
+    }
+   
+
+}
