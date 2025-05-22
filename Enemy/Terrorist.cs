22c0eb86dd428f647ead11f3585659bf3081830b
menachem yarhi <nbjo460@gmail.com>
2025-05-22 15:39:08 +0300

    finished soldiers

--- Enemy/Terrorist.cs
+++ Enemy/Terrorist.cs
@@ -4,43 +4,21 @@ namespace IDF_Operation.Enemy
 {
     public class Terrorist : Soldier
     {
-        public string Name
-        {
-            get; set
-            {
-                if (Name == null) { Name = value; }
-            }
-        }
+        public override string Name => base.Name;
 
-        public int Rank { get; set { if (value <= 5 && value >= 1) { Rank = value; } } }
+        public override int Rank => base.Rank;
 
         public bool IsAlive { get; set; }
         
-        public Location currentLocation;
         public List<Weapon> Weapons { get; private set; }
 
-        public Terrorist(int _rank, Weapon _weapon)
+        public Terrorist(int _rank, Weapon _weapon) : base(_rank, _weapon)
         {
-            Weapons = new List<Weapon>();
             IsAlive = true;
-            Rank = _rank;
-            Weapons.Add(_weapon);
         }
-        public Terrorist(int _rank, List<Weapon> _weapons)
+        public Terrorist(int _rank, List<Weapon> _weapons) : base (_rank, _weapons)
         {
-            Weapons = new List<Weapon>();
             IsAlive = true;
-            Rank = _rank;
-            Weapons.AddRange(_weapons);
-        }
-        public void AddWeapon(Weapon _weapon)
-        {
-            Weapons.Add(_weapon);
-        }
-
-        public void AddLocation(Location _location)
-        {
-            currentLocation = _location;
         }
 
 
--- General/Soldier.cs
+++ General/Soldier.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace IDF_Operation.General
 {
-    public abstract class Soldier
+    public class Soldier
     {
-        string Name { get; set; }
-        int Rank { get; set; }
+        public virtual string Name
+        {
+            get; set
+            {
+                if (Name == null) { Name = value; }
+            }
+        }
+        public virtual int Rank { get; set { if (value <= 5 && value >= 1) { Rank = value; } } }
 
-        public List<Weapon> Weapons { get; private set { } }
-         
+        public virtual List<Weapon> Weapons { get; private set { } }
+
+        public virtual Location CurrentLocation { get; private set; }
+
+        public virtual void AddWeapon(Weapon _weapon)
+        {
+            Weapons.Add(_weapon);
+        }
+
+        public virtual void AddLocation(Location _location)
+        {
+            currentLocation = _location;
+        }
+
+        public Soldier(int _rank, Weapon _weapon)
+        {
+            Weapons = new List<Weapon>();
+            Rank = _rank;
+            Weapons.Add(_weapon);
+        }
+        public Soldier(int _rank, List<Weapon> _weapons)
+        {
+            Weapons = new List<Weapon>();
+            Rank = _rank;
+            Weapons.AddRange(_weapons);
+        }
+
     }
 }
