3cc58e7937f4f5453331d5629cf6c8fd7e60f58f
menachem yarhi <nbjo460@gmail.com>
2025-05-22 12:10:08 +0300

    fixed the Report. By Mistake i replaced the reports

--- General/Report/IntelligenceReport.cs
+++ General/Report/IntelligenceReport.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using IDF_Operation.Enemy;
+using System;
 
 public class IntelligenceReport : Report
 {
-	public IntelligenceReport()
-	{
-	}
+    public override int ReportId { get { return ReportId; } set { if (ReportId == null) { ReportId = value; } } }
 
+    public Terrorist TerroristPersonality { get; }
+
+    public Location LocationPersonality { get; }
+
+    public Time TimePeronality { get; }
+    public IntelligenceReport(int id, Terrorist _terrorist, Location _location)
+    {
+        LocationPersonality = _location;
+        TerroristPersonality = _terrorist;
+        ReportId = id;
+    }
 }
