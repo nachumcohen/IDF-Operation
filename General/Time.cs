00d23319d5c1089ca82755cbf868c65cde550f48
menachem yarhi <nbjo460@gmail.com>
2025-05-22 16:47:16 +0300

    make time

--- General/Time.cs
+++ General/Time.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
-public class Time
+public class Times
 {
-	public Time()
+	public string Format = "dd.MM.yyyy hh:mm:ss";
+	
+	public string Time { get; private set; }
+	public Times(string _format = "")
 	{
+		if (_format != "")
+		{
+			Format = _format;
+		}
+		Time = CreateTimeSign();
 	}
+
+	public void ResetFormat()
+	{
+		Format = "dd.MM.yyyy hh.mm.ss";
+    }
+	public string CreateTimeSign()
+	{
+		return DateTime.Now.ToString(Format);
+	}
+    public override string ToString()
+    {
+        return Time;
+    }
+
 }
