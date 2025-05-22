9a990e1455dc419de6e67f0c19884ba269759171
menachem yarhi <nbjo460@gmail.com>
2025-05-22 12:39:06 +0300

    set TaskReport

--- General/Report/TaskReport.cs
+++ General/Report/TaskReport.cs
@@ -7,10 +7,16 @@ public class TaskReport : Report
 
 	public static int ReportId { get { return ReportId; } private set { ReportId = value;} }
 
+	public IntelligenceReport IntelligenceReportSpecific { get { return IntelligenceReportSpecific; } private set { IntelligenceReportSpecific = value; } }
 	
-	public TaskReport()
+	public Weapon WeaponSpecific { get { return WeaponSpecific; } private set { WeaponSpecific = value; } }
+
+	public bool Success;
+	public TaskReport(IntelligenceReport _intelligenceReport, Weapon _weaponSpecicfic)
 	{
 		ReportId += 1;
+		IntelligenceReportSpecific = _intelligenceReport;
+		WeaponSpecific = _weaponSpecicfic;
 	}
 
 }
